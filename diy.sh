46b0b2d293fbdf61fd1f22feed4951a485281a8f
Zhibo <z.io@qq.com>
2020-04-14 16:36:06 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,9 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.123.2/g' package/base-files/files/bin/config_generate
+# 添加软件包
+git clone https://github.com/Lienol/openwrt-package package/lienol
+git clone https://github.com/vernesong/OpenClash package/openclash
+git clone https://github.com/jefferymvp/luci-app-koolproxyR package/koolproxyR
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/adguardhome
+git clone https://github.com/frainzy1477/luci-app-clash package/clash
